e4615f77fad16ad3a8f6a66830bc43e98aa772ad
Danijo <Danieljosuemu@gmail.com>
2015-04-28 20:28:05 +0100

    plus CSV save
    write CSV

--- pre/wordscript2.R
+++ pre/wordscript2.R
@@ -87,50 +87,55 @@ isValidDestination <- function(t,breakrule=F){
 }
 
 branches <- function() {
-	docs <- new.env()
-	docs$text <- list()
-	docs$redirects <- list()
-	docs$father <- list()
-	docs$type <- list()
-	docs$quality <- list()
-	docs$geo <- list()
-
+	docs <- new.env()  # inserir em env é ainda mais eficiente que list(), parece
+	redirects <- new.env()
+	father <- new.env()
+	type <- new.env()
+	articlequality<-new.env()
+	geo<-new.env()
 	pagenumber <<- 0
 	page <- function(node) {  # queremos ler as <page>
 		n <- xmlChildren(node)
-		pagenumber <<- pagenumber+1
+		pagenumber <<- pagenumber+1  # n percebo porque ele dá sempre 1
 		if(all(c('title','revision') %in% names(n))) {
 			title <- xmlValue(n[['title']])
 			if(!grepl(':', title)) {
-				n <- xmlChildren(n[['revision']])
+				revision <- n[['revision']]
+				n <- xmlChildren(revision)
 								
 				if('text' %in% names(n)) {
+
 					print(pagenumber)
 					print(title)
-
 					# o parser repete chaves "text", dai temos que juntá-las
 					t <- paste(sapply(n[names(n) == 'text'], xmlValue), collapse=' ')
-					tt <- transform_text(t)
-
+					transformedText <-transform_text(t)
+					#print(transformedText)
 					validDestination<-isValidDestination(substitute(t))
 
-					if (validDestination!="false" && length(tt)>100) {
-						docs$text[title] <- tt
+					# FIXME: acho que o Rui verificava que o texto tinha um certo
+					# tamanho. ie: length(t)>100
+					if (validDestination!="false" && length(transformedText)>100)
+					{
+						docs[[title]] <- transformedText
 
-						validDestination <- strsplit(validDestination,",")
-						docs$quality[[title]] <- validDestination[0]
-						docs$type[[title]] <- validDestination[1]
+						validDestination<- strsplit(validDestination,",")
+						articlequality[[title]]<-validDestination[0]
+						type[[title]]<-validDestination[1]
+						
 					}
-					docs$father[[title]]<- getFather(t)
-					docs$geo[[title]]<-getGeo(t)
+					father[[title]]<- getFather(t)
+					geo[[title]]<-getGeo(t)
 					redirectDestination<-isRedirect(t)
 					if (redirectDestination!= "false")
-						docs$redirects[redirectDestination] <- c(docs$redirects[[redirectDestination]],title)
+					{
+						redirects[[redirectDestination]]<-c(redirects[[redirectDestination]],title)
+					}
 				}
 			}
 		}
 	}
-	list(page=page, getDocs=function() as.list(docs))
+	list(page=page, getDocs=function() as.list(docs),getRedirects=function() as.list(redirects),getFathers=function() as.list(father),getType=function() as.list(type),getQuality=function() as.list(articlequality),getGeo=function() as.list(geo))
 }
 
 
@@ -143,28 +148,32 @@ xmlEventParse('xml', branches=b, useTagName=FALSE, addContext=FALSE, ignoreBlank
 ####################
 
 print("A gerar a matriz de documento-termo ...")
-d <- b$getDocs()
-corpus <- Corpus(VectorSource(d$text))
+docs <- b$getDocs()
+corpus <- Corpus(VectorSource(docs))
 save(corpus, file='corpus.RData')
 
 print("Para matriz documento-termo ...")
 dtm <- DocumentTermMatrix(corpus, control=list(wordLengths=c(2,10)))
 print("- Remover esparsos ...")
-dtm <- removeSparseTerms(dtm, 0.992)
+dtm <- removeSparseTerms(dtm, 0.995)
 print("- Transformações da matriz ...")
 #dtm <- weightTfIdf(dtm)
 dtm <- weightSMART(dtm, 'ltc')
 save(dtm, file='dtm.RData')
 
-#print("Gravar ...")
-#m <- as.matrix(dtm)
-#rownames(m) <- names(docs)
-#save(m, file='m.RData')
-#write.csv(m, 'm.csv')
-
-print("... obtendo dados de destinos...")
-sorted_doc_sizes <- sort(as.data.frame(lapply(docs$text,length)), TRUE)
-
-print("..gravando dados de destinos...")
-save(sorted_doc_sizes,d$redirects,d$fathers,d$type,d$quality,d$geo,file="destinationData.RData")
-print("Gravar matriz final...")
+print("Gravar ...")
+m <- as.matrix(dtm)
+rownames(m) <- names(docs)
+redirects<-b$getRedirects()
+fathers<-b$getFathers()
+type<-b$getType
+quality<-b$getQuality
+geo<-b$getGeo
+save(redirects,fathers,type,quality,geo,file="destinationData.RData")
+save(m, file='m.RData')
+write.csv(m, 'm.csv')
+write.csv(redirects, 'redirects.csv')
+write.csv(fathers, 'fathers.csv')
+write.csv(type, 'type.csv')
+write.csv(quality, 'quality.csv')
+write.csv(geo, 'geo.csv')
