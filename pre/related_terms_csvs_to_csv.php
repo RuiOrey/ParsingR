515a01143890febec074dab9fc6ccf1a519617e6
Rui Orey <rui.albuquerque.dorey@gmail.com>
2015-05-25 11:28:09 +0100

    related words

---
+++ pre/related_terms_csvs_to_csv.php
@@ -0,0 +1,39 @@
+<?php 
+//edit this path to correct csv path. TODO : change to argument
+$csv_dir="./adjacent_terms/";
+
+$files = array();
+$lines = array();
+$newlin="";
+foreach (glob($csv_dir."*.csv") as $filename) {
+    // Open the file
+    if( $handle = @fopen( $filename, "r") )
+    {
+        $x = 0; // Start the line counter
+        $newEntry="";
+
+        // Cycle each line until end or reach the lines to return limit
+        while(! feof( $handle ) )
+ 
+        {           
+
+        	$line = fgets($handle); // Read the line
+            $lessons[$filename][] = $line;            
+ 
+        	if ($x!=0)
+        	{
+        	$newEntry.="\"".basename($filename,".csv")."\"".",".$line;
+            
+			}
+            $x++; // Increase the counter
+		}
+		$newlin.=$newEntry;
+		echo basename($filename,".csv")."\n";
+        //echo $newlin;
+        }
+
+        $fp = fopen("./related_words.csv","wb");
+		fwrite($fp,$newlin);
+		fclose($fp);
+}
+ ?>
